54c545b32bb0b7b1f4d60fa260869a7a4589d87b
SSshadow99 <45866647+SSshadow99@users.noreply.github.com>
2018-12-25 13:59:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -362,7 +362,7 @@ client.on('message', message => {
         .setDescription('**برفكس البوت  (  1  )**')
         .addField('play', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
-        .addField('disconnect', 'الخروج من رومك الصوتي')
+        .addField('stop', 'الخروج من رومك الصوتي')
         .addField('skip', 'تخطي الأغنية')
         .addField('pause', 'ايقاف الاغنية مؤقتا')
         .addField('resume', 'تكملة الاغنية')
